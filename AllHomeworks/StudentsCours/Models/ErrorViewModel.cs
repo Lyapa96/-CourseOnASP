9aacd8d896a4590eef37289a5a08dd3c49d12af3
lms <lms@skbkontur.ru>
2017-11-19 21:13:02 +0500

    fix names of folder and solution

---
+++ AllHomeworks/StudentsCours/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentsCours.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
